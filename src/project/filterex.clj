fa3c4e4265ac0f3356fbcfba01c4618d8acb149b
sailajaappari <sailajakadali@gmail.com>
2016-05-03 13:18:10 +0530

    modified filter

--- src/project/filterex.clj
+++ src/project/filterex.clj
@@ -1,12 +1,19 @@
 (defn filter-vals 
   "To perform the filter operation using recursion"
-  [nums]
+  [f nums]
   (loop [n nums res []]
      (if (empty? n)
-        res
-        (recur (rest n) (if (= (rem (first n) 2) 0) (conj res (first n)) res)))))
+        res       
+        (if (= f "even?")
+          (recur (rest n) (if (= (rem (first n) 2) 0)
+                             (conj res (first n))
+                             res))
+          (recur (rest n) (if (> (rem (first n) 2) 0)
+                             (conj res (first n))
+                             res))))))
+(filter-vals "even?" [1 2 3 4 5 6 7 8])
+(filter-vals "odd?" (range 10))
 
-(filter-vals [1 2 3 4 5 6 7 8])
-(filter-vals (range 10))
+
 
 
