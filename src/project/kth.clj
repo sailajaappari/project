8ecb1bed293f33c4f16d301429f7c4a72ac8f8fc
sailajaappari <sailajakadali@gmail.com>
2016-05-05 17:54:33 +0530

    Modified last last but one and kth functions

--- src/project/kth.clj
+++ src/project/kth.clj
@@ -1,7 +1,9 @@
 (defn find-kth
   "find kth element of a list with out recursion "
   [nums x]
-  (get nums x))
+  (if (< (count nums) x)
+     nil
+     (first (subvec nums x))))
 
 (find-kth [10 12 14 25 20 15 18] 3)
 (find-kth [10 12 5] 3)
--- src/project/last-but-one.clj
+++ src/project/last-but-one.clj
@@ -1,6 +1,10 @@
 (defn find-last-but-one
   "Find the last but one element of a coll"
   [nums]
-  (get nums (- (count nums) 2)))
+  (if (= (count nums) 0)
+     nil 
+     (first (subvec nums (- (count nums) 2)))))
 (find-last-but-one [19 16 10 25 26])
 (find-last-but-one [])
+
+
