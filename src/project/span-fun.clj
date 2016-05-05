e0d2349c9cac9f0c7623aede4736fab0b1f20445
sailajaappari <sailajakadali@gmail.com>
2016-05-05 16:15:17 +0530

    Modified span function

--- src/project/span-fun.clj
+++ src/project/span-fun.clj
@@ -5,3 +5,13 @@
 (span 2 [1 2 3 4 1 2 3 4]) 
 (span 9 [1 2 3])
 (span 0 [1 2 3])
+
+
+(defn span1 
+  [x nums]
+  (split-with (partial > (second x)) nums))
+
+(span1 '(< 3) [1 2 3 4 1 2 3 4]) 
+(span1 '(< 9) [1 2 3])
+(span1 '(< 0) [1 2 3])
+(span1 '(< 0) [-1 2 3 -2])
