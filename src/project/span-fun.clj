0e0b4957cd4ca145a2d835d71d5a15b93393ff3a
sailajaappari <sailajakadali@gmail.com>
2016-05-06 13:20:02 +0530

    Modified span function

--- src/project/span-fun.clj
+++ src/project/span-fun.clj
@@ -15,3 +15,15 @@
 (span1 '(< 9) [1 2 3])
 (span1 '(< 0) [1 2 3])
 (span1 '(< 0) [-1 2 3 -2])
+
+
+
+(defn span2
+  [vals]
+  (letfn [(take-while-fun [vals1]
+            (take-while (partial = (first (vec vals1))) (vec vals1)))
+          (drop-while-fun [vals1]
+            (drop-while (partial = (first (vec vals1))) (vec vals1)))]
+         [(take-while-fun vals) (drop-while-fun vals)]))
+(span2 '(1 1 2 3 3))
+(span2 '(a a a b c c d d))
