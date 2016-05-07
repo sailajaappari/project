00d2b6285f787d1528845a07c78ccff6f29703cb
sailajaappari <sailajakadali@gmail.com>
2016-05-07 14:17:30 +0530

    Modified version of take-while and drop-while functions

--- src/project/drop-while-fun.clj
+++ src/project/drop-while-fun.clj
@@ -1,17 +1,15 @@
-(defn pred
-  [x val]
-  (x val))
+
 (defn drop-while-vals
   "Implement drop-while function "
   [f nums]
   (loop [nums1 nums res [] cnt 0]
     (if (>= cnt (count nums))
-       res
-       (if (pred f (first nums1))
+       (apply concat (list res))
+       (if (f (first nums1))
           (recur (rest nums1) res (inc cnt))
           (conj res nums1)))))
 (drop-while-vals neg? [-1 -2 0 1 2 3])
-(drop-while-vals pos? [1 -2 3])
+(drop-while-vals pos? [1 -2 3]) 
 (drop-while-vals even? [2 4 1 2 3 0])
 (drop-while-vals odd? [1 2 4 5])
 (drop-while-vals odd? [])
--- src/project/take-while-fun.clj
+++ src/project/take-while-fun.clj
@@ -1,18 +1,20 @@
-(defn pred
-  [x val]
-  (x val))
+
 (defn take-while-vals
   "Implement take-while function"
   [f nums]
-  (loop [nums1 nums res []]
-     (if-not (pred f (first nums1))
-        res
-        (recur (rest nums1) (conj res (first nums1))))))
+  (if (empty? nums)
+    '()
+    (loop [nums1 nums res []]
+     (if-not (f (first nums1))
+        (apply concat (list res))
+        (recur (rest nums1) (conj res (first nums1)))))))
 
 (take-while-vals neg? [-1 -2 0 -1 2 3])
 (take-while-vals odd? [1 3 4 5 6 7]) 
 (take-while-vals pos? [1 -1 2 -2])
 (take-while-vals even? [1 2 3 4]) 
+(take-while-vals odd? [])
+
 
 
 
